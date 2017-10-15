580911047aa285159179b4660afb328d0cbc3800
Tim <7452348+timlueg@users.noreply.github.com>
2017-10-15 17:59:51 +0200

    SimpleHillClimber

---
+++ SimpleHillClimber/fitClimber.m
@@ -0,0 +1,33 @@
+function [solution,xValues,yValues] = fitClimber()
+%UNTITLED4 Summary of this function goes here
+% Detailed explanation goes here
+fit = @(x) (x>1).*(2.*x-2) + (x<=0.5).*8.*x + ((x>0.5).*(x<=1)).*(-8.*x + 8);
+
+numIterations = 100;
+
+xValues = zeros(1,numIterations);
+yValues = zeros(1,numIterations);
+
+%random value in interval (a,b)
+a = 0;
+b = 2;
+currentX = a + (b-a) .* rand();
+
+for i=1:numIterations
+    solutionRange = [-0.1, 0, 0.1] + currentX;
+    [maxfit, indexMax] = max(fit(solutionRange));
+    currentX = solutionRange(indexMax);
+    
+    xValues(i) = currentX;
+    yValues(i) = maxfit;
+end
+
+solution=yValues(numIterations);
+figure(3);
+fplot(fit, [-0.5, 12],'c', 'lineWidth', 2)
+hold on
+plot(xValues, yValues,'--r', 'LineWidth',2)
+plot(xValues(numIterations), yValues(numIterations), 'bo', 'LineWidth',1)
+hold off
+
+end
---
+++ SimpleHillClimber/visualization.m
@@ -0,0 +1,16 @@
+solutions=zeros(1,100);
+plotX=zeros(100,100);
+plotY=zeros(100,100);
+fit = @(x) (x>1).*(2.*x-2) + (x<=0.5).*8.*x + ((x>0.5).*(x<=1)).*(-8.*x + 8);
+for i=1:100
+    [solutions(i),plotX(i,:),plotY(i,:)]=fitClimber();
+end
+figure(1);
+h1=histogram(solutions,40);
+xlabel('X Werte');
+ylabel('absolute Häufigkeit');
+figure(2);
+boxplot(solutions);
+ax=gca;
+ax.XTickLabel = {'100 HillClimb Ergebnisse'};
+ylabel('X Werte');
