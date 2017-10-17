d02549a6b92f09a944b4b5ca9e745b77f44d9ddf
JanUrf <janurfei@gmail.com>
2017-10-17 11:56:24 +0200

    add Boxplot and HIstogram
    Zwei Grafiken hinzugefügt und die Schleife, die die 100 HillClimbs
    ausführt parallel gemacht, da die einzelnen HillClimbs unabhängig
    voneinander sind.

--- SimpleHillClimber/visualization.m
+++ SimpleHillClimber/visualization.m
@@ -2,7 +2,7 @@ solutions=zeros(1,100);
 plotX=zeros(100,100);
 plotY=zeros(100,100);
 fit = @(x) (x>1).*(2.*x-2) + (x<=0.5).*8.*x + ((x>0.5).*(x<=1)).*(-8.*x + 8);
-for i=1:100
+parfor i=1:100
     [solutions(i),plotX(i,:),plotY(i,:)]=fitClimber();
 end
 figure(1);
