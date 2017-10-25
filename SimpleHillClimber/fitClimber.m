dedc9b115f31dc1f264ed4fa1c472f04fad1fa10
JanUrf <janurfei@gmail.com>
2017-10-25 13:21:32 +0200

    fitClimb dimension variable

--- SimpleHillClimber/fitClimber.m
+++ SimpleHillClimber/fitClimber.m
@@ -4,24 +4,35 @@ function [solution,xValues,yValues] = fitClimber()
 fit = @(x) (x>1).*(2.*x-2) + (x<=0.5).*8.*x + ((x>0.5).*(x<=1)).*(-8.*x + 8);
 
 numIterations = 100;
+dimensionen=1;
 
-xValues = zeros(1,numIterations);
-yValues = zeros(1,numIterations);
+xValues = zeros(dimensionen,numIterations);
+yValues = zeros(dimensionen,numIterations);
+current=zeros(1,dimensionen);
 
 %random value in interval (a,b)
 a = 0;
 b = 2;
-currentX = a + (b-a) .* rand();
-
+for i=1:dimensionen
+    current(i) = a + (b-a) .* rand();
+end
 for i=1:numIterations
-    solutionRange = [-0.1, 0, 0.1] + currentX;
+    solutionRange = [0;0.1;-0.1] + current(1,:);
     [maxfit, indexMax] = max(fit(solutionRange));
-    currentX = solutionRange(indexMax);
+    current(1,:) = solutionRange(indexMax,:);
     
-    xValues(i) = currentX;
+    xValues(:,i) = current(1,:)';
     yValues(i) = maxfit;
 end
 
-solution=xValues(numIterations);
+solution=xValues(:,numIterations);
 
+end
+function [y] = egg(xx)
+% xx has two dimensions/columns: n x 2
+x1 = xx(:,1);
+x2 = xx(:,2);
+term1 = -(x2+47) .* sin(sqrt(abs(x2+x1./2+47)));
+term2 = -x1 .* sin(sqrt(abs(x1-(x2+47))));
+y = term1 + term2;
 end
