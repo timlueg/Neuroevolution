f976f8e283c29955897e7f0d39ac2568d0dbb6d2
Tim <7452348+timlueg@users.noreply.github.com>
2017-11-07 21:05:33 +0100

    GA label for graphs

--- SimpleGeneticAlgorithm/visualization.m
+++ SimpleGeneticAlgorithm/visualization.m
@@ -1,4 +1,3 @@
-%[history,fitnessHistory,populationSize,numIterations]=eggGa();
 [history,fitnessHistory,populationSize,numIterations]=eggGA_vectorized();
 
 numExperiments = 100;
@@ -7,12 +6,14 @@ AllMedian=zeros(numExperiments,numIterations);
 AllWorst=zeros(numExperiments,numIterations);
 
 for i=1:numExperiments
-[history,fitnessHistory,populationSize,numIterations]=eggGa();
+%[history,fitnessHistory,populationSize,numIterations]=eggGa();
+[history,fitnessHistory,populationSize,numIterations]=eggGA_vectorized();
 AllElite(i,:)=min(fitnessHistory,[],2)';
 AllMedian(i,:)=median(fitnessHistory,2)';
 AllWorst(i,:)=max(fitnessHistory,[],2)';
-
 end
+
+%mean for every iteration over all experiments
 eliteDevelopment=mean(AllElite,1);
 medianDevelopment=mean(AllMedian,1);
 worstDevelopment=mean(AllWorst,1);
@@ -24,7 +25,12 @@ plot(eliteDevelopment);
 plot(medianDevelopment);
 plot(worstDevelopment);
 legend('Elite','Median','Worst');
+xlabel('Iteration');
+ylabel('egg value');
 hold off;
 
 figure(2);
+hold on;
 boxplot(AllElite(:,numIterations));
+ylabel('egg value')
+hold off;
