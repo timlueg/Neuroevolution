c942c66c0ee1dc4c0a96e80750e5d6efa1fb18c2
Tim <7452348+timlueg@users.noreply.github.com>
2017-11-30 17:21:05 +0100

    ESP training iterations definion

--- ESP/heartRatePrediction.m
+++ ESP/heartRatePrediction.m
@@ -1,14 +1,23 @@
+load ('trainingData.mat','tdata')
+
+training_data = {tdata{1:10}};
+test_data = {tdata{10:12}};
+
+numTraining = size(training_data,2);
+numTest = size(test_data,2);
+
 num_inputNodes = 2;
 num_innnerNodes = 3;
 num_outputNodes = 1;
-SubpopulationSize = 20;
-count_nodes_insertion =10;
+SubpopulationSize = 6;
+num_nodes_insertion =10;
 
 num_allNodes = num_innnerNodes + num_inputNodes + num_outputNodes;
 num_of_Subpop = num_innnerNodes + num_outputNodes;
 
 %gewichteMatrix = zeros(num_allNodes,num_of_Subpop);
 population = zeros(SubpopulationSize,num_allNodes,num_of_Subpop);
+population_fitness = zeros(SubpopulationSize,1,num_of_Subpop);
 
 %zusammenstellung der Population
 for i=1:num_of_Subpop
@@ -17,13 +26,33 @@ for i=1:num_of_Subpop
     end
 end
 
-N = SubpopulationSize;
-perm{count_nodes_insertion} = [];
-for i=1:count_nodes_insertion
-    p = randperm(N);
-    perm{j} = [perm{j} p];
+
+for i=1:numTraining
+    
+    N = SubpopulationSize;
+    %population_perm = zeros(SubpopulationSize * num_nodes_insertion ,num_allNodes,num_of_Subpop);
+    for j=1:num_nodes_insertion
+        for k=1:num_of_Subpop
+            p = randperm(N);
+            
+            for l=1:SubpopulationSize
+            
+            %population_perm(1+(j-1)*SubpopulationSize:i*SubpopulationSize,:,k) = population(p,:,k);
+            %combine along thid dimension 
+            selected_weights = reshape(permute(population(p,:,k),[2,1,3]),size(population(p,:,k),2),[])';
+            startInput = [training_data{numTraining}(1,1),training_data{numTraining}(1,3), 1, 1, 1];
+            for m=1:size(training_data{numTraining},1)
+                startInput * selected_weights';
+            end
+            
+            end
+
+
+        end
+    end
+
 end
 
-
-
-
+function [error]= fitness(target,out)
+error=0.5* (target-out)^2;
+end
