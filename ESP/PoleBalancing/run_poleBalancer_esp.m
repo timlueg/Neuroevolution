e6f6ea117869f2139df55dbffadb0e67a83877ac
Tim <7452348+timlueg@users.noreply.github.com>
2017-12-05 00:10:59 +0100

    esp folder structure

---
+++ ESP/PoleBalancing/run_poleBalancer_esp.m
@@ -0,0 +1,43 @@
+function [fitness, steps] = run_poleBalancer_esp()
+clear;
+headless = true;    % removes visualizations from the simulation
+cfg = twoPole;        % contains the simulation configuration
+SystemInit;         % some Matlab initialization
+
+% This should point to your activation function, which will receive these
+% parameters. Feel free to change the structure if you need more!
+% ind      ind object, containing weights, configuration, etc.
+% input    will contain the input plus the bias on the input layer. Feel
+%          free to ignore the bias if you handle it in a different way
+cfg.activate = @activate_feedforward; 
+
+%% Configure your algorithm
+%% !This is just an example. Please use your *own* configuration variables
+cfg.num_inputs = cfg.inputs;cfg.num_outputs = cfg.outputs;
+cfg.num_layers = 1;
+cfg.num_hid_per_lay = 2;
+cfg.mu_init = 0.1;
+cfg.actFn = @tanh;
+
+
+%% Run your algorithm
+runs = 10000;
+for i=1:runs
+    %% !insert your network here
+    ind = initialize_feedforward(cfg);    
+    if ~headless
+        % the 'ind' object will be inserted into your activation function!
+        [fitness(i), steps(i)] = twoPole_test( ind, cfg, 'vis'); 
+        figure(999);semilogy(steps');hold on;semilogy(fitness');
+        drawnow;
+        figure(2);
+    else
+        [fitness(i), steps(i)] = twoPole_test( ind, cfg); 
+    end
+    disp(['Steps achieved: ' int2str(steps(end)) ', corresponding to fitness: ' num2str(fitness(end))]);
+    disp(['Max steps was: ' int2str(max(steps))]);
+end
+
+figure(999);semilogy(steps');hold on;semilogy(fitness');
+
+end
