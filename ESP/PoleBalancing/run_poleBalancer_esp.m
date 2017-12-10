404fc8de4b71c8ca67d73bc3c02e4e97bd9f7782
Tim <7452348+timlueg@users.noreply.github.com>
2017-12-10 23:21:56 +0100

    ESP remove cpu bottleneck

--- ESP/PoleBalancing/run_poleBalancer_esp.m
+++ ESP/PoleBalancing/run_poleBalancer_esp.m
@@ -3,7 +3,7 @@ function [fitness, steps] = run_poleBalancer_esp(W, act)
 %clear;
 headless = true;    % removes visualizations from the simulation
 cfg = twoPole;        % contains the simulation configuration
-SystemInit;         % some Matlab initialization
+%SystemInit;         % some Matlab initialization
 
 % This should point to your activation function, which will receive these
 % parameters. Feel free to change the structure if you need more!
@@ -21,8 +21,6 @@ cfg.activate = @activate_recurrent;
 % cfg.mu_init = 0.1;
 % cfg.actFn = @tanh;
 
-runs = 1;
-for i=1:runs
     %% !insert your network here
     %ind = poleBalancer_esp.initialize_recurrent(cfg);
     ind.W = W;
@@ -30,7 +28,7 @@ for i=1:runs
     ind.actFn = @tanh;
     if ~headless
         % the 'ind' object will be inserted into your activation function!
-        [fitness(i), steps(i)] = twoPole_test( ind, cfg, 'vis'); 
+        [fitness, steps] = twoPole_test( ind, cfg, 'vis'); 
         %figure(999);
         %semilogy(steps');
         hold on;
@@ -38,11 +36,11 @@ for i=1:runs
         drawnow;
         figure(2);
     else
-        [fitness(i), steps(i)] = twoPole_test( ind, cfg); 
+        [fitness, steps] = twoPole_test( ind, cfg); 
     end
     %disp(['Steps achieved: ' int2str(steps(end)) ', corresponding to fitness: ' num2str(fitness(end))]);
     %disp(['Max steps was: ' int2str(max(steps))]);
-end
+
 % 
 % figure(999);
 % semilogy(steps');
