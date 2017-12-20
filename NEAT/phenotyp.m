0abe5b9bd9207b93c70b03b180289992d9f1e874
JanUrf <janurfei@gmail.com>
2017-12-20 19:58:26 +0100

    Versuch der Verbesserung der Phenotyperzeugung

--- NEAT/phenotyp.m
+++ NEAT/phenotyp.m
@@ -5,15 +5,22 @@ function [gewichtsmatrix] = phenotyp(nodeList,connectionList)
 size_knoten = size(nodeList,1);
 size_connections = size(connectionList,1);
 
+index = find(connectionList(:,4));
+connectionList=connectionList(index,:);
+
 gewichtsmatrix = zeros(nodeList,nodeList);
 
+indexGewichtsmatrix= sub2ind(size(gewichtsmatrix),connectionList(:,1),connectionList(:,2));
 %in einer Spalte der Gewichtsmatrix stehen alle Inputgewichte des Knoten,
 %Knoten der in der Spalte repräsentiert wird.
-for i=1:size_connections
-    if connectionList(i,4)
-        gewichtsmatrix(connectionList(i,1),connectionList(i,2))= connectionList(i,3);
-    end
-end
+
+gewichtsmatrix(indexGewichtsmatrix)=connectionList(:,3);
+
+% for i=1:size_connections
+%     if connectionList(i,4)
+%         gewichtsmatrix(connectionList(i,1),connectionList(i,2))= connectionList(i,3);
+%     end
+% end
 
 end
 
