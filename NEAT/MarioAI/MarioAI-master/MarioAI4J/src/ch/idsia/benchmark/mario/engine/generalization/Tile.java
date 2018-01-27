38c0488b2faf0f94ba3a257c68bc23cade0d252e
timlueg <7452348+timlueg@users.noreply.github.com>
2018-01-27 16:07:51 +0100

    use double tile ids representing tile importance/similarity.

--- NEAT/MarioAI/MarioAI-master/MarioAI4J/src/ch/idsia/benchmark/mario/engine/generalization/Tile.java
+++ NEAT/MarioAI/MarioAI-master/MarioAI4J/src/ch/idsia/benchmark/mario/engine/generalization/Tile.java
@@ -4,19 +4,19 @@ public enum Tile {
 	/**
 	 * ZLevel: 0 only 
 	 */
-	CANNON_MUZZLE("CM", 14, 0),
+	CANNON_MUZZLE("CM", 0.98, 0),
 	
 	/**
 	 * ZLevel: 0 only 
 	 */
-	CANNON_TRUNK("CT", 2, 0),
+	CANNON_TRUNK("CT", 0.97, 0),
 	
 	/**
 	 * ZLevel: 0, 1 and 2
 	 * 
 	 * Represents a coin Mario can collect.
 	 */
-	COIN_ANIM("C", 3, 0, 1, 2),
+	COIN_ANIM("C", 0.4, 0, 1, 2),
 	
 	/**
 	 * ZLevel: 0 only
@@ -26,7 +26,7 @@ public enum Tile {
      * -- brick with a hidden coin
 	 * -- brick with a hidden friendly flower
 	 */
-	BREAKABLE_BRICK("BB", 4, 0),
+	BREAKABLE_BRICK("BB", 0.6, 0),
 	
 	/**
 	 * ZLevel: 0 only
@@ -36,7 +36,7 @@ public enum Tile {
 	 * -- question brick containing flower/mushroom
 	 * -- question brick containing 0-N coins inside
 	 */
-	QUESTION_BRICK("BQ", 5, 0),
+	QUESTION_BRICK("BQ", 0.8, 0),
 	
 	/**
 	 * ZLevel: 1 only
@@ -49,7 +49,7 @@ public enum Tile {
 	 * -- question brick, contains flower/mushroom
 	 * -- question brick, 0-N coins inside
 	 */
-	BRICK("B", 6, 1),
+	BRICK("B", 1, 1),
 	
 	/**
 	 * ZLevel: 0 only
@@ -62,49 +62,49 @@ public enum Tile {
 	 * -- question brick, contains flower/mushroom
 	 * -- question brick, 0-N coins inside
 	 */
-	FLOWER_POT("FP", 7, 0),
+	FLOWER_POT("FP", 0.92, 0),
 	
 	/**
 	 * ZLevel: 0, 1, 2
 	 * 
 	 * Represents a SOLID block.
 	 */
-	BORDER_CANNOT_PASS_THROUGH("BI", 8, 0, 1, 2),
+	BORDER_CANNOT_PASS_THROUGH("BI", 0.9, 0, 1, 2),
 	
 	/**
 	 * ZLevel: 0, 1
 	 * 
 	 * Represents a block you can stand on but also jump through (when jumping up) but not fall through.
 	 */
-	BORDER_HILL("BH", 9, 0, 1),
+	BORDER_HILL("BH", 1, 0, 1),
 	
 	/**
 	 * ZLevel: 1 only
 	 * 
 	 * Flower pot or a cannon.
 	 */
-	FLOWER_POT_OR_CANNON("PC", 10, 1),
+	FLOWER_POT_OR_CANNON("PC", 0.91, 1),
 	
 	/**
 	 * ZLevel: 0, 1
 	 * 
 	 * Represents a LADDER block (but not its top)
 	 */
-	LADDER("L", 11, 0, 1),
+	LADDER("L", 0.1, 0, 1),
 	
 	/**
 	 * ZLevel: 0, 1
 	 * 
 	 * Represents top of the ladder (you cannot climb up anymore than this).
 	 */
-	TOP_OF_LADDER("TL", 12, 0, 1),
+	TOP_OF_LADDER("TL", 0.11, 0, 1),
 	
 	/**
 	 * ZLevel: 0, 1, 2
 	 * 
 	 * PRINCESS! Congratulation, you will win if you reach her!
 	 */
-	PRINCESS("P", 13, 1, 2),
+	PRINCESS("P", 0, 1, 2),
 
 	/**
 	 * ZLevel: 2 only
@@ -122,17 +122,17 @@ public enum Tile {
 		
 	private String debug;
 	
-	private int code;
+	private double code;
 
 	private int[] zLevels;
 	
-	private Tile(String debug, int code, int... zLevels) {
+	private Tile(String debug, double code, int... zLevels) {
 		this.debug = debug;
 		this.code = code;
 		this.zLevels = zLevels;
 	}
 
-	public int getCode() {
+	public double getCode() {
 		return code;
 	}
 	
